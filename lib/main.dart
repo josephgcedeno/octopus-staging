e7e18601f4480787348ab35e83a3eb23ab5fc4de
josephgcedeno <josephgcedeno@gmail.com>
2023-02-08 10:22:45 +0800

    feat: initialize parse server

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,23 @@ import 'package:octopus/configs/themes.dart';
 import 'package:octopus/infrastructures/repository/quote_repository.dart';
 import 'package:octopus/module/dashboard/interfaces/screens/controller_screen.dart';
 import 'package:octopus/module/time_record/service/cubit/quote_cubit.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   /// Load env file
   await dotenv.load();
+
+  final String appId = dotenv.get('APP_ID');
+  final String serverUrl = dotenv.get('SERVER_URL');
+  final String masterKey = dotenv.get('MASTER_KEY');
+
+  await Parse().initialize(
+    appId,
+    serverUrl,
+    masterKey: masterKey,
+  );
 
   runApp(const App());
 }
