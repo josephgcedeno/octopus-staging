f53ad7a9bbf7437ab9768a2667e15a708413a1f5
josephgcedeno <josephgcedeno@gmail.com>
2023-02-10 14:41:28 +0800

    refactor: use api response  and api error response

--- lib/infrastructures/repository/interfaces/auth_repository.dart
+++ lib/infrastructures/repository/interfaces/auth_repository.dart
@@ -1,5 +1,5 @@
+import 'package:octopus/infrastructures/models/api_response.dart';
 import 'package:octopus/infrastructures/models/auth/auth_request.dart';
-import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 abstract class IAuthRepository {
   /// This function will be use to login user.
@@ -9,7 +9,7 @@ abstract class IAuthRepository {
   /// [password] password provided to the account to authenticate.
   ///
   /// [email] email provided to the account to authenticate. It is nullable since username and password would be enough for authenticating.
-  Future<ParseResponse> loginUser(AuthLoginRequest payload);
+  Future<APIResponse<void>> loginUser(AuthLoginRequest payload);
 
   /// This function will be use to register user account.
   ///
@@ -20,18 +20,18 @@ abstract class IAuthRepository {
   /// [name] the user's fullname and not necessarily required for signing up, it's a extra field for user table.
   ///
   /// [position] the user's current position in the company and not necessarily required for signing up, it's a extra field for user table.
-  Future<ParseResponse> signUpUser(AuthRegisterRequest payload);
+  Future<APIResponse<void>> signUpUser(AuthRegisterRequest payload);
 
   /// This function will be use in the future for resetting the password.
   ///
   /// [email] this is where the recovery mail will be sent and create a new password.
-  Future<ParseResponse> resetPassword({
+  Future<APIResponse<void>> resetPassword({
     required String email,
   });
 
   /// This function will validate if the user is already logged in. It will verify if the token is expired or not. If expired, the user will need to login again.
-  Future<bool> isLoggedIn();
+  Future<APIResponse<void>> isLoggedIn();
 
   /// This function will logout current user and session.
-  Future<ParseResponse> logout();
+  Future<APIResponse<void>> logout();
 }
