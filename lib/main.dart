b152a10542e9ee4817ae7fd93b003ae653bf8ecf
josephgcedeno <josephgcedeno@gmail.com>
2023-02-13 10:26:27 +0800

    chore: include TimeRecordCubit and create new date on initialize app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:octopus/configs/themes.dart';
 import 'package:octopus/infrastructures/repository/auth_repository.dart';
-import 'package:octopus/module/login/interfaces/screens/login_screen.dart';
+import 'package:octopus/infrastructures/repository/time_in_out_repository.dart';
 import 'package:octopus/module/login/service/cubit/login_cubit.dart';
+import 'package:octopus/module/time_record/interfaces/screens/time_record_screen.dart';
+import 'package:octopus/module/time_record/service/cubit/time_record_cubit.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 void main() async {
@@ -26,18 +28,40 @@ void main() async {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final AuthRepository authRepository = AuthRepository();
+  State<App> createState() => _AppState();
+}
 
+class _AppState extends State<App> {
+  final AuthRepository authRepository = AuthRepository();
+  final TimeInOutRepository timeInOutRepository = TimeInOutRepository();
+
+  Future<void> initializeData() async {
+    /// Create daily record.
+    await timeInOutRepository.createNewDate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: <BlocProvider<dynamic>>[
         BlocProvider<LoginCubit>(
           create: (BuildContext context) =>
               LoginCubit(authRepository: authRepository),
+        ),
+        BlocProvider<TimeRecordCubit>(
+          create: (BuildContext context) => TimeRecordCubit(
+            timeInOutRepository: timeInOutRepository,
+          ),
         ),
       ],
       child: MaterialApp(
@@ -56,6 +80,6 @@ class App extends StatelessWidget {
 class _HomePageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const LoginScreen();
+    return const TimeRecordScreen();
   }
 }
