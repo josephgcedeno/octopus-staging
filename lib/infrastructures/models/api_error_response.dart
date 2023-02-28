5d62dba6b7e5aa6cfbab8b300eee99ceb42c922e
josephgcedeno <josephgcedeno@gmail.com>
2023-02-28 11:22:45 +0800

    fix: use factory function APIErrorResponse to prevent type cast error

--- lib/infrastructures/models/api_error_response.dart
+++ lib/infrastructures/models/api_error_response.dart
@@ -3,20 +3,11 @@ class APIErrorResponse {
     required this.message,
     required this.errorCode,
   });
-  
-  factory APIErrorResponse.socketErrorRes({
-    required String message,
-    required String errorCode,
-  }) =>
-      APIErrorResponse(errorCode: errorCode, message: message);
 
-  static Map<String, dynamic> socketErrorResponse() {
-    return <String, dynamic>{
-      'success': false,
-      'message': 'No Internet Connection',
-      'errorCode': 'NO_INTERNET_CONNECTION',
-    };
-  }
+  factory APIErrorResponse.socketErrorResponse() => APIErrorResponse(
+        errorCode: 'NO_INTERNET_CONNECTION',
+        message: 'No Internet Connection',
+      );
 
   /// base API response
   final String message;
