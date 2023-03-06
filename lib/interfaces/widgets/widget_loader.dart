e11361827e1f153170debf1c7ffa730e011254d2
josephgcedeno <josephgcedeno@gmail.com>
2023-03-06 16:50:15 +0800

    feat: add fade in effect in DTR

---
+++ lib/interfaces/widgets/widget_loader.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:octopus/configs/themes.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:shimmer/shimmer.dart';
+
+/// These are the static widgets that we're gonna use as skeleton loaders.
+/// We use the Text Widget with dummy values to emulate the the exact dimensions of specific widgets
+/// The text values won't show up because they are overlapped by the shimmer effect. It's just for height and width purposes.
+
+const Duration fadeInDuration = Duration(milliseconds: 500);
+
+Widget clockLoader(BuildContext context) {
+  final ThemeData theme = Theme.of(context);
+  final double width = MediaQuery.of(context).size.width;
+  final double height = MediaQuery.of(context).size.height;
+  
+  return Container(
+    padding: const EdgeInsets.all(8.0),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      shape: BoxShape.circle,
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          color: theme.primaryColor.withAlpha(30),
+          blurRadius: 30,
+        ),
+      ],
+    ),
+    child: CircularPercentIndicator(
+      radius: height * 0.15,
+      lineWidth: 15.0,
+      backgroundColor: theme.primaryColor.withAlpha(15),
+      progressColor: kBlue.withAlpha(30),
+      reverse: true,
+      center: ClipRRect(
+        borderRadius: BorderRadius.circular(width),
+        child: Shimmer.fromColors(
+          baseColor: Colors.white,
+          highlightColor: Colors.black12,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.black38,
+            ),
+            height: height,
+          ),
+        ),
+      ),
+    ),
+  );
+}
