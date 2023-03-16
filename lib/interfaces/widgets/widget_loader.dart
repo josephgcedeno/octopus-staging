089b612720d56dbaef7d903e1b0640fbd34f9208
josephgcedeno <josephgcedeno@gmail.com>
2023-03-16 15:52:37 +0800

    fix: update base color

--- lib/interfaces/widgets/widget_loader.dart
+++ lib/interfaces/widgets/widget_loader.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:octopus/configs/themes.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -7,33 +6,25 @@ import 'package:shimmer/shimmer.dart';
 /// We use the Text Widget with dummy values to emulate the the exact dimensions of specific widgets
 /// The text values won't show up because they are overlapped by the shimmer effect. It's just for height and width purposes.
 
-const Color shimmerBase = Colors.white;
-const Color shimmerGlow = Colors.black12;
+const Color shimmerBase = Color(0xffFAF9FC);
+const Color shimmerGlow = Color(0xFFE2E1E2);
 
 const Duration fadeInDuration = Duration(milliseconds: 500);
 
 Widget clockLoader(BuildContext context) {
-  final ThemeData theme = Theme.of(context);
   final double width = MediaQuery.of(context).size.width;
   final double height = MediaQuery.of(context).size.height;
 
-  return Container(
-    padding: const EdgeInsets.all(8.0),
-    decoration: BoxDecoration(
+  return DecoratedBox(
+    decoration: const BoxDecoration(
       color: Colors.white,
       shape: BoxShape.circle,
-      boxShadow: <BoxShadow>[
-        BoxShadow(
-          color: theme.primaryColor.withAlpha(30),
-          blurRadius: 30,
-        ),
-      ],
     ),
     child: CircularPercentIndicator(
       radius: height * 0.15,
       lineWidth: 15.0,
-      backgroundColor: theme.primaryColor.withAlpha(15),
-      progressColor: kBlue.withAlpha(30),
+      backgroundColor: Colors.transparent,
+      progressColor: Colors.transparent,
       reverse: true,
       center: ClipRRect(
         borderRadius: BorderRadius.circular(width),
@@ -43,7 +34,7 @@ Widget clockLoader(BuildContext context) {
           child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8),
-              color: Colors.black38,
+              color: shimmerGlow,
             ),
             height: height,
           ),
@@ -64,7 +55,7 @@ Widget lineLoader({
     child: Container(
       decoration: BoxDecoration(
         borderRadius: withRadius ? BorderRadius.circular(8) : null,
-        color: Colors.white12,
+        color: shimmerBase,
       ),
       width: width,
       height: height,
