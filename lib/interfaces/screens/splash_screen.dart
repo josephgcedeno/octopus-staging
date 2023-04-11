a51c55622a8cac75bc0b295a47fdeb6e6ff3003c
Mae Krisine Vannirrie Sarmiento <msarmiento_190000001023@uic.edu.ph>
2023-04-11 16:49:39 +0800

    refactor: withdraw from splash gradient background

--- lib/interfaces/screens/splash_screen.dart
+++ lib/interfaces/screens/splash_screen.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:octopus/configs/themes.dart';
-import 'package:octopus/interfaces/widgets/splash_gradient_background.dart';
 
 /// Splash screen, only shows for a few milliseconds. fades out to authentication_screen afterwards.
 class SplashScreen extends StatefulWidget {
@@ -39,13 +38,27 @@ class _SplashScreenState extends State<SplashScreen> {
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
 
-    return SplashGradientBackground(
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.only(top: height * 0.05),
-          width: width * 0.35,
-          height: height * 0.20,
-          child: SvgPicture.asset(whiteLogoSvg),
+    return Scaffold(
+      body: Container(
+        width: width,
+        height: height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: <Color>[
+              gradient1,
+              gradient2,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.only(top: height * 0.05),
+            width: width * 0.35,
+            height: height * 0.20,
+            child: SvgPicture.asset(whiteLogoSvg),
+          ),
         ),
       ),
     );
