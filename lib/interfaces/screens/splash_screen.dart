0a57905619d7442c5a800c99cdda1e7d4d8da7f3
Mae Krisine Vannirrie Sarmiento <msarmiento_190000001023@uic.edu.ph>
2023-04-11 16:58:57 +0800

    refactor: convert into stateless widget

--- lib/interfaces/screens/splash_screen.dart
+++ lib/interfaces/screens/splash_screen.dart
@@ -1,37 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:octopus/configs/themes.dart';
 
 /// Splash screen, only shows for a few milliseconds. fades out to authentication_screen afterwards.
-class SplashScreen extends StatefulWidget {
+
+class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
-
-  static const String routeName = '/splash';
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  double opacityLevel = 1.0;
-
-  Timer interval = Timer(const Duration(seconds: 1), () {});
-
-  @override
-  void initState() {
-    super.initState();
-    interval = Timer.periodic(const Duration(seconds: 1), (_) {
-      setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    interval.cancel();
-  }
 
   @override
   Widget build(BuildContext context) {
