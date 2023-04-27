57e715ed53443297b811aeee8cccf2e5be1e35e8
josephgcedeno <josephgcedeno@gmail.com>
2023-04-27 12:07:33 +0800

    feat: add documentation with the repository

--- lib/infrastructures/repository/interfaces/user_repository.dart
+++ lib/infrastructures/repository/interfaces/user_repository.dart
@@ -2,6 +2,27 @@ import 'package:octopus/infrastructures/models/api_response.dart';
 import 'package:octopus/infrastructures/models/user/user_response.dart';
 
 abstract class IUserRepository {
+  /// FOR ADMIN USE ONLY
+  ///
+  /// Fetches a list of users from the API, filtered by the given parameters.
+  ///
+  /// [nuxifyId] - An optional string representing the Nuxify ID of the user.
+  ///
+  /// [firstName] - An optional string representing the first name of the user.
+  ///
+  /// [lastName] - An optional string representing the last name of the user.
+  ///
+  /// [currentPosition] - An optional string representing the current position of the user.
+  ///
+  /// [isAdmin] - An optional string representing whether the user is an admin.
+  ///
+  /// [address] - An optional string representing the address of the user.
+  ///
+  /// [civilStatus] - An optional string representing the civil status of the user.
+  ///
+  /// [age] - An optional integer representing the age of the user.
+  ///
+  /// [isDeactive] - An optional boolean representing whether the user is inactive.
   Future<APIListResponse<User>> fetchAllUser({
     String? nuxifyId,
     String? firstName,
@@ -11,9 +32,36 @@ abstract class IUserRepository {
     String? address,
     String? civilStatus,
     int? age,
-    bool? isDeactive
+    bool? isDeactive,
   });
 
+  /// FOR ADMIN USE ONLY
+  ///
+  /// Creates a new user in the API with the given information.
+  ///
+  /// [firstName] - The first name of the new user.
+  ///
+  /// [lastName] - The last name of the new user.
+  ///
+  /// [nuxifyId] - The Nuxify ID of the new user.
+  ///
+  /// [birthDate] - The birth date of the new user.
+  ///
+  /// [address] - The address of the new user.
+  ///
+  /// [civilStatus] - The civil status of the new user.
+  ///
+  /// [dateHired] - The date that the new user was hired.
+  ///
+  /// [imageSource] - The source of the new user's profile image.
+  ///
+  /// [pagIbigNo] - The Pag-IBIG number of the new user.
+  ///
+  /// [sssNo] - The SSS number of the new user.
+  ///
+  /// [tinNo] - The TIN number of the new user.
+  ///
+  /// [philHealtNo] - The PhilHealth number of the new user.
   Future<APIListResponse<User>> createUser({
     required String firstName,
     required String lastName,
@@ -29,6 +77,35 @@ abstract class IUserRepository {
     required String philHealtNo,
   });
 
+  /// FOR ADMIN USE ONLY
+  ///
+  /// Updates the user with the given [id] in the API.
+  ///
+  /// [id] - The ID of the user to update.
+  ///
+  /// [firstName] - (Optional) The updated first name of the user.
+  ///
+  /// [lastName] - (Optional) The updated last name of the user.
+  ///
+  /// [nuxifyId] - (Optional) The updated Nuxify ID of the user.
+  ///
+  /// [birthDate] - (Optional) The updated birth date of the user.
+  ///
+  /// [address] - (Optional) The updated address of the user.
+  ///
+  /// [civilStatus] - (Optional) The updated civil status of the user.
+  ///
+  /// [dateHired] - (Optional) The updated date that the user was hired.
+  ///
+  /// [imageSource] - (Optional) The updated source of the user's profile image.
+  ///
+  /// [pagIbigNo] - (Optional) The updated Pag-IBIG number of the user.
+  ///
+  /// [sssNo] - (Optional) The updated SSS number of the user.
+  ///
+  /// [tinNo] - (Optional) The updated TIN number of the user.
+  ///
+  /// [philHealtNo] - (Optional) The updated PhilHealth number of the user.
   Future<APIListResponse<User>> updateUser({
     required String id,
     String? firstName,
@@ -45,6 +122,11 @@ abstract class IUserRepository {
     String? philHealtNo,
   });
 
+  /// FOR ADMIN USE ONLY
+  ///
+  /// Deactivates the user with the given [id] in the API.
+  ///
+  /// [id] - The ID of the user to deactivate.
   Future<APIListResponse<User>> deactivateUser({
     required String id,
   });
