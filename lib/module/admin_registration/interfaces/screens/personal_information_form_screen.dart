4357208b69f3cfef819977028b38cc91c490d007
garcia-christian <cgarcia_190000000526@uic.edu.ph>
2023-05-03 14:36:35 +0800

    chore: removed warnings

--- lib/module/admin_registration/interfaces/screens/personal_information_form_screen.dart
+++ lib/module/admin_registration/interfaces/screens/personal_information_form_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:octopus/interfaces/widgets/appbar.dart';
 import 'package:octopus/module/admin_registration/interfaces/screens/ids_form_screen.dart';
