86d389d6abdea18c2cd3d0185c6810a124f534b8
josephgcedeno <josephgcedeno@gmail.com>
2023-05-04 11:41:49 +0800

    refactor: add progress percentage with upload status and refactor as a variable for testable function

--- lib/interfaces/widgets/upload_progress.dart
+++ lib/interfaces/widgets/upload_progress.dart
@@ -8,11 +8,19 @@ import 'package:octopus/internal/debug_utils.dart';
 import 'package:octopus/internal/error_message_string.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-Future<String> uploadFile({
+typedef UploadStringFunction = Future<String> Function({
+  required BuildContext context,
+  required File file,
+  String? fileName,
+});
+
+UploadStringFunction uploadFile = ({
   required BuildContext context,
   required File file,
   String? fileName,
 }) async {
+  final ValueNotifier<double> progressNotifier = ValueNotifier<double>(0);
+
   double progress = 0;
   dynamic setState1;
   showDialog<void>(
@@ -26,31 +34,66 @@ Future<String> uploadFile({
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    CircularProgressIndicator(
-                      value: progress,
-                      backgroundColor: Colors.grey[300],
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        Theme.of(context).primaryColor,
-                      ),
-                      strokeWidth: 10.0,
-                      semanticsLabel: '${progress.toInt()}% uploaded',
-                    ),
-                    Center(
-                      child: Text(
-                        '${progress.toInt()}%',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ),
-                  ],
+                SizedBox(
+                  width: 75,
+                  height: 75,
+                  child: LayoutBuilder(
+                    builder: (context, constrains) {
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            width: constrains.maxWidth,
+                            height: constrains.maxHeight,
+                            child: ValueListenableBuilder(
+                              valueListenable: progressNotifier,
+                              builder: (context, double value, _) {
+                                return CircularProgressIndicator(
+                                  value: value,
+                                  valueColor: AlwaysStoppedAnimation<Color>(
+                                    value < 0.5
+                                        ? Colors.green
+                                        : value > 0.8
+                                            ? Colors.greenAccent
+                                            : Colors.green.shade400,
+                                  ),
+                                  backgroundColor: Colors.grey[300],
+                                  strokeWidth: 10.0,
+                                  semanticsLabel:
+                                      '${(value * 100).toInt()}% uploaded',
+                                );
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: constrains.maxWidth * 0.90,
+                            height: constrains.maxHeight * 0.90,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                child: Text(
+                                  '${progress.toInt()}%',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: progress > 90
+                                        ? Colors.green
+                                        : Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                 ),
-                const SizedBox(height: 20),
-                const Text('Uploading file...'),
+                const Padding(
+                  padding: EdgeInsets.only(top: 15.0),
+                  child: Text('Uploading file...'),
+                ),
               ],
             ),
           );
@@ -67,6 +110,7 @@ Future<String> uploadFile({
     progressCallback: (int count, int total) {
       setState1(() {
         progress = (count / total) * 100;
+        progressNotifier.value = progress / 100;
       });
     },
   );
@@ -88,4 +132,4 @@ Future<String> uploadFile({
   }
 
   throw errorSomethingWentWrong;
-}
+};
