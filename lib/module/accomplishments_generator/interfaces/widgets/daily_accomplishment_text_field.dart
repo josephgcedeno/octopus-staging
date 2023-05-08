f569be0970622bebe7016e0320d8d19e07946207
josephgcedeno <josephgcedeno@gmail.com>
2023-05-08 14:52:34 +0800

    fix: align text on web and add debounce

--- lib/module/accomplishments_generator/interfaces/widgets/daily_accomplishment_text_field.dart
+++ lib/module/accomplishments_generator/interfaces/widgets/daily_accomplishment_text_field.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class DailyAccomplishmentTextField extends StatefulWidget {
@@ -15,22 +18,30 @@ class _DailyAccomplishmentTextFieldState
     extends State<DailyAccomplishmentTextField> {
   final TextEditingController textController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    textController.addListener(() {
-      setState(() {
-        widget.name(textController.text);
+  // For delay on change
+  Timer? _debounce;
+
+  // On change top text field (source wallet) will be handled by this function.
+  void _onChangeTopTextField(String input) {
+    if (_debounce?.isActive ?? false) {
+      _debounce!.cancel();
+    } // New changes will cancel the previous request
+
+    if (input.isNotEmpty) {
+      _debounce = Timer(const Duration(milliseconds: 1500), () {
+        print('puasok');
+        widget.name(input);
       });
-    });
+    }
   }
 
   @override
   void dispose() {
     textController.dispose();
+    _debounce?.cancel();
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
@@ -39,18 +50,22 @@ class _DailyAccomplishmentTextFieldState
 
     return Container(
       height: height * 0.03,
-      width: width * 0.3,
+      width: kIsWeb ? width * 0.15 : width * 0.3,
       margin: EdgeInsets.only(bottom: height * 0.01),
       child: TextField(
         controller: textController,
+        onChanged: _onChangeTopTextField,
         style: TextStyle(
           color: theme.primaryColor,
         ),
         decoration: InputDecoration(
+          isDense: true,
           focusColor: theme.primaryColor,
-          contentPadding: EdgeInsets.symmetric(
-            vertical: height * 0.009,
-          ),
+          contentPadding: kIsWeb
+              ? const EdgeInsets.only(bottom: 5, top: 3)
+              : EdgeInsets.symmetric(
+                  vertical: height * 0.006,
+                ),
           border: UnderlineInputBorder(
             borderSide: BorderSide(color: theme.primaryColor),
           ),
