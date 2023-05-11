9b1a5fa1f9ea20af400526a0808507deb9298eae
josephgcedeno <josephgcedeno@gmail.com>
2023-05-11 14:50:18 +0800

    chore: create fetch current user to prevent direct access to package

--- lib/infrastructures/repository/user_repository.dart
+++ lib/infrastructures/repository/user_repository.dart
@@ -70,7 +70,7 @@ class UserRepository extends IUserRepository {
             errorCode: '409',
           );
         }
-        
+
         final int birthDateEpoch = epochFromDateTime(date: birthDate);
         final int dateHiredEpoch = epochFromDateTime(date: dateHired);
 
@@ -553,13 +553,73 @@ class UserRepository extends IUserRepository {
         }
       }
 
-      if (user != null && user.get<bool>(usersIsAdminField)!) {}
       String errorMessage = errorSomethingWentWrong;
       if (user != null && !user.get<bool>(usersIsAdminField)!) {
         errorMessage = errorInvalidPermission;
       }
       throw APIErrorResponse(
         message: errorMessage,
+        errorCode: null,
+      );
+    } on SocketException {
+      throw APIErrorResponse.socketErrorResponse();
+    }
+  }
+
+  @override
+  Future<APIResponse<UseWithrRole>> fetchCurrentUser() async {
+    try {
+      final ParseUser? user = await ParseUser.currentUser() as ParseUser?;
+
+      if (user != null) {
+        // Save the user to the Parse Server
+        final EmployeeInfoParseObject employeeInfoParseObject =
+            EmployeeInfoParseObject();
+
+        final ParseResponse userAccountResponse =
+            await employeeInfoParseObject.getObject(user.objectId!);
+
+        if (userAccountResponse.error != null) {
+          formatAPIErrorResponse(error: userAccountResponse.error!);
+        }
+
+        if (userAccountResponse.success &&
+            userAccountResponse.results != null) {
+          final EmployeeInfoParseObject employeeRecord =
+              EmployeeInfoParseObject.toCustomParseObject(
+            data: userAccountResponse.results!.first,
+          );
+
+          return APIResponse<UseWithrRole>(
+            success: true,
+            message: 'Successfully updated user info.',
+            data: UseWithrRole(
+              address: employeeRecord.address,
+              birthDateEpoch: employeeRecord.birthDateEpoch,
+              civilStatus: employeeRecord.civilStatus,
+              dateHiredEpoch: employeeRecord.dateHiredEpoch,
+              firstName: employeeRecord.firstName,
+              id: employeeRecord.objectId!,
+              isDeactive: employeeRecord.isDeactive,
+              lastName: employeeRecord.lastName,
+              nuxifyId: employeeRecord.nuxifyId,
+              pagIbigNo: employeeRecord.pagIbigNo,
+              philHealtNo: employeeRecord.philHealthNo,
+              position: employeeRecord.position,
+              profileImageSource: employeeRecord.profileImageSource,
+              sssNo: employeeRecord.sssNo,
+              tinNo: employeeRecord.tinNo,
+              userRole: user.get<bool>(usersIsAdminField)!
+                  ? UserRole.admin
+                  : UserRole.client,
+            ),
+            errorCode: null,
+          );
+        }
+      }
+
+      throw APIErrorResponse(
+        message: errorSomethingWentWrong,
         errorCode: null,
       );
     } on SocketException {
