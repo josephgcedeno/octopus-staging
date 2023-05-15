a479e6ad7c3d25d1aff63baedd93825b3fa017a0
josephgcedeno <josephgcedeno@gmail.com>
2023-05-15 22:59:57 +0800

    feat: on isRestrictTwoWeeksMax is true, limit the number of days to be query

--- lib/interfaces/widgets/date_and_time_picker.dart
+++ lib/interfaces/widgets/date_and_time_picker.dart
@@ -13,11 +13,17 @@ class DateTimePicker<T> extends StatefulWidget {
     required this.type,
     required this.callBack,
     this.showLabel = true,
+    this.isRestrictTwoWeeksMax = true,
+    this.isRestrictPreviousDay = true,
+    this.maximumDay = const Duration(days: 14),
     Key? key,
   }) : super(key: key);
   final PickerType type;
   final void Function(T from, T to) callBack;
   final bool showLabel;
+  final bool isRestrictTwoWeeksMax;
+  final bool isRestrictPreviousDay;
+  final Duration maximumDay;
 
   @override
   State<DateTimePicker<T>> createState() => _DateTimePickerState<T>();
@@ -66,18 +72,38 @@ class _DateTimePickerState<T> extends State<DateTimePicker<T>> {
     }
 
     // Prevent users from setting past dates
-    if (res.isBefore(DateTime.now().subtract(const Duration(days: 1)))) {
+    if (widget.isRestrictPreviousDay &&
+        res.isBefore(DateTime.now().subtract(const Duration(days: 1)))) {
       showSnackBar(
         message:
             'Please enter a valid date range starting from today or a future date.',
       );
       fromTextController.clear();
       toTextController.clear();
+      return;
     }
 
     /// Trigger callback if both fields are filled.
     if (fromTextController.text.isNotEmpty &&
         toTextController.text.isNotEmpty) {
+      /// Check if the number of days exceeds to 2weeks
+      if (widget.isRestrictTwoWeeksMax) {
+        final DateTime fromCastToDateTime = from as DateTime;
+        final DateTime toCastToDateTime = to as DateTime;
+        final Duration difference =
+            toCastToDateTime.difference(fromCastToDateTime);
+
+        if (difference.inDays > widget.maximumDay.inDays) {
+          showSnackBar(
+            message:
+                'The date range between two date exceeds the maximum days specified ${widget.maximumDay.inDays}, given is ${difference.inDays}',
+          );
+          fromTextController.clear();
+          toTextController.clear();
+        }
+
+        return;
+      }
       widget.callBack.call(from, to);
     }
   }
