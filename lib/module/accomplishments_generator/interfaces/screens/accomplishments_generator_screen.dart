92299f1ad703f4f7bc511099aab971e0e369f6f6
josephgcedeno <josephgcedeno@gmail.com>
2023-05-23 22:36:51 +0800

    feat: dynamically increase button on web

--- lib/module/accomplishments_generator/interfaces/screens/accomplishments_generator_screen.dart
+++ lib/module/accomplishments_generator/interfaces/screens/accomplishments_generator_screen.dart
@@ -5,6 +5,7 @@ import 'package:octopus/configs/themes.dart';
 import 'package:octopus/infrastructures/models/dsr/dsr_response.dart';
 import 'package:octopus/interfaces/widgets/appbar.dart';
 import 'package:octopus/internal/debug_utils.dart';
+import 'package:octopus/internal/screen_resolution_utils.dart';
 import 'package:octopus/module/accomplishments_generator/interfaces/screens/daily_accomplishment_report_screen.dart';
 import 'package:octopus/module/accomplishments_generator/interfaces/widgets/accomplishments_tasks_slider_and_list.dart';
 import 'package:octopus/module/accomplishments_generator/service/cubit/accomplishments_cubit.dart';
@@ -76,25 +77,28 @@ class _AccomplishmentsGeneratorScreenState
                       );
                     }
                   },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: width * 0.06,
-                      vertical: height * 0.02,
-                    ),
-                    margin: EdgeInsets.symmetric(
-                      vertical: height * 0.02,
-                      horizontal: width * 0.04,
-                    ),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: theme.primaryColor,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Text(
-                      'Proceed',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: kWhite,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.06,
+                        vertical: height * 0.02,
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        vertical: height * 0.02,
+                        horizontal: width * 0.04,
+                      ),
+                      width: kIsWeb && width > smWebMinWidth ? null : width,
+                      decoration: BoxDecoration(
+                        color: theme.primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        'Proceed',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: kWhite,
+                        ),
                       ),
                     ),
                   ),
