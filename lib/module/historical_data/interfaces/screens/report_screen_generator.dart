7c5c732998c844da0d8c0119c1120659cc175619
josephgcedeno <josephgcedeno@gmail.com>
2023-05-25 13:01:25 +0800

    feat: add responsiveness on pdf generator screen

--- lib/module/historical_data/interfaces/screens/report_screen_generator.dart
+++ lib/module/historical_data/interfaces/screens/report_screen_generator.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:octopus/configs/themes.dart';
 import 'package:octopus/interfaces/screens/pdf_viewer_screen.dart';
 import 'package:octopus/interfaces/widgets/upload_progress.dart';
+import 'package:octopus/internal/screen_resolution_utils.dart';
 import 'package:octopus/internal/string_helper.dart';
 import 'package:octopus/module/historical_data/interfaces/widgets/export_pdf_button.dart';
 import 'package:octopus/module/historical_data/interfaces/widgets/report_screen_page_generator.dart';
@@ -52,7 +53,7 @@ class _ReportScreenGeneratorState extends State<ReportScreenGenerator> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final double height = MediaQuery.of(context).size.height;
-    final double width = kIsWeb ? 600 : MediaQuery.of(context).size.width;
+    final double width = MediaQuery.of(context).size.width;
 
     return BlocListener<HistoricalCubit, HistoricalState>(
       listenWhen: (HistoricalState previous, HistoricalState current) =>
@@ -73,7 +74,7 @@ class _ReportScreenGeneratorState extends State<ReportScreenGenerator> {
         }
       },
       child: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: const Color(0xffECECEC),
         appBar: AppBar(
           backgroundColor: kWhite,
           elevation: 0,
@@ -82,60 +83,67 @@ class _ReportScreenGeneratorState extends State<ReportScreenGenerator> {
             onPressed: () => Navigator.of(context).pop(),
           ),
         ),
-        body: SizedBox(
-          width: width,
-          height: height * 0.87,
-          child: Padding(
+        body: Center(
+          child: Container(
+            width: kIsWeb && width > smWebMinWidth ? width * 0.50 : width,
             padding: EdgeInsets.only(
               left: width * 0.04,
               right: width * 0.04,
               bottom: height * 0.02,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Column(
+            color: Colors.white,
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constrains) {
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        SizedBox(
-                          width: width * 0.30,
-                          height: height * 0.055,
-                          child: Image.asset(nuxifyLogoPng),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: <Widget>[
-                              FittedBox(
-                                fit: BoxFit.scaleDown,
-                                child: Text(
-                                  '$reportTitle ${widget.leaveType != null ? '- ${widget.leaveType!.toTitleCase()}' : ''}',
-                                ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            SizedBox(
+                              height: constrains.maxHeight * 0.055,
+                              child: Image.asset(
+                                nuxifyLogoPng,
+                                height: 67.63,
                               ),
-                              Text(
-                                widget.reportDate,
-                                style: theme.textTheme.bodySmall,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  FittedBox(
+                                    fit: BoxFit.scaleDown,
+                                    child: Text(
+                                      '$reportTitle ${widget.leaveType != null ? '- ${widget.leaveType!.toTitleCase()}' : ''}',
+                                    ),
+                                  ),
+                                  Text(
+                                    widget.reportDate,
+                                    style: theme.textTheme.bodySmall,
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
+                    const Expanded(
+                      child: ReportScreenPageGenerator(),
+                    ),
+                    ExportPDFButton(
+                      dateReport: widget.reportDate,
+                      title:
+                          '$reportTitle ${widget.leaveType != null ? '- ${widget.leaveType!.toTitleCase()}' : ''}',
+                    ),
                   ],
-                ),
-                const Expanded(
-                  child: ReportScreenPageGenerator(),
-                ),
-                ExportPDFButton(
-                  dateReport: widget.reportDate,
-                  title:
-                      '$reportTitle ${widget.leaveType != null ? '- ${widget.leaveType!.toTitleCase()}' : ''}',
-                ),
-              ],
+                );
+              },
             ),
           ),
         ),
