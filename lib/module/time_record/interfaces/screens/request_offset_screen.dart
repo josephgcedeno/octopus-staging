10fe5cc8d9c5e98b6417379841bb8b0103394a40
josephgcedeno <josephgcedeno@gmail.com>
2023-05-29 18:10:01 +0800

    feat: responsive request offset screen

--- lib/module/time_record/interfaces/screens/request_offset_screen.dart
+++ lib/module/time_record/interfaces/screens/request_offset_screen.dart
@@ -5,6 +5,7 @@ import 'package:octopus/configs/themes.dart';
 import 'package:octopus/interfaces/widgets/appbar.dart';
 import 'package:octopus/interfaces/widgets/date_and_time_picker.dart';
 import 'package:octopus/internal/debug_utils.dart';
+import 'package:octopus/internal/screen_resolution_utils.dart';
 import 'package:octopus/module/time_record/service/cubit/time_record_cubit.dart';
 
 class RequestOffsetScreen extends StatefulWidget {
@@ -77,6 +78,7 @@ class _RequestOffsetScreenState extends State<RequestOffsetScreen> {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: const GlobalAppBar(leading: LeadingButton.back),
@@ -126,130 +128,153 @@ class _RequestOffsetScreenState extends State<RequestOffsetScreen> {
         },
         child: GestureDetector(
           onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-          child: SingleChildScrollView(
-            child: Container(
-              height: height * 0.87,
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding:
-                              EdgeInsets.only(bottom: height * 0.03, top: 20),
-                          child: Text(
-                            'Request Offset',
-                            style: kIsWeb
-                                ? theme.textTheme.titleLarge
-                                : theme.textTheme.titleMedium?.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            EdgeInsets.only(bottom: height * 0.03, top: 20),
+                        child: Text(
+                          'Request Offset',
+                          style: theme.textTheme.titleMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Text(
-                          'I’ll be away the next working day...',
-                          style: theme.textTheme.bodyLarge,
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(
-                            top: 20,
-                            bottom: 13,
-                          ),
-                          width: double.infinity,
-                          child: DateTimePicker<TimeOfDay>(
-                            type: PickerType.time,
-                            callBack: (TimeOfDay from, TimeOfDay to) {
-                              fromTime = from;
-                              toTime = to;
-                            },
-                          ),
-                        ),
-                        Row(
+                      ),
+                      Text(
+                        'I’ll be away the next working day...',
+                        style: theme.textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Align(
+                      child: SizedBox(
+                        width: kIsWeb && width > smWebMinWidth
+                            ? width * 0.45
+                            : width,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(right: 3.0),
-                              child: Icon(
-                                Icons.info,
-                                color: blackColor,
-                                size: 15,
+                            Column(
+                              children: <Widget>[
+                                Container(
+                                  margin: const EdgeInsets.only(
+                                    top: 20,
+                                    bottom: 13,
+                                  ),
+                                  width: double.infinity,
+                                  child: DateTimePicker<TimeOfDay>(
+                                    type: PickerType.time,
+                                    callBack: (TimeOfDay from, TimeOfDay to) {
+                                      fromTime = from;
+                                      toTime = to;
+                                    },
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(right: 3.0),
+                                      child: Icon(
+                                        Icons.info,
+                                        color: blackColor,
+                                        size: 15,
+                                      ),
+                                    ),
+                                    Text(
+                                      maximumTimeText,
+                                      style:
+                                          theme.textTheme.bodySmall?.copyWith(
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 15),
+                                  child: TextFormField(
+                                    controller: reasonTextController,
+                                    maxLines: 8,
+                                    minLines: 8,
+                                    validator: (String? value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Fields cannot be empty.';
+                                      }
+                                      return null;
+                                    },
+                                    keyboardType: TextInputType.multiline,
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(8),
+                                        borderSide: BorderSide.none,
+                                      ),
+                                      hintText:
+                                          'Write reason (e.g. Process personal documents)',
+                                      hintStyle: theme.textTheme.bodySmall,
+                                      filled: true,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: kIsWeb && width > smWebMinWidth
+                                  ? width * 0.45
+                                  : width,
+                              margin: EdgeInsets.only(bottom: height * 0.03),
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 0,
+                                  backgroundColor: theme.primaryColor,
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: kIsWeb ? 23 : 10,
+                                    horizontal: 15,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      8,
+                                    ),
+                                  ),
+                                ),
+                                onPressed: isLoading
+                                    ? null
+                                    : () {
+                                        if (_formKey.currentState!.validate()) {
+                                          saveOffset();
+                                        }
+                                      },
+                                child: isLoading
+                                    ? const SizedBox(
+                                        width: 25,
+                                        height: 25,
+                                        child: CircularProgressIndicator(),
+                                      )
+                                    : Text(
+                                        'Request',
+                                        style:
+                                            theme.textTheme.bodyLarge?.copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
                               ),
                             ),
-                            Text(
-                              maximumTimeText,
-                              style: theme.textTheme.bodySmall?.copyWith(
-                                fontStyle: FontStyle.italic,
-                              ),
-                            )
                           ],
                         ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 15),
-                          child: TextFormField(
-                            controller: reasonTextController,
-                            maxLines: 8,
-                            minLines: 8,
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Fields cannot be empty.';
-                              }
-                              return null;
-                            },
-                            keyboardType: TextInputType.multiline,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(8),
-                                borderSide: BorderSide.none,
-                              ),
-                              hintText:
-                                  'Write reason (e.g. Process personal documents)',
-                              hintStyle: theme.textTheme.bodySmall,
-                              filled: true,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: height * 0.03),
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          elevation: MaterialStateProperty.all(0),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                          ),
-                        ),
-                        onPressed: isLoading
-                            ? null
-                            : () {
-                                if (_formKey.currentState!.validate()) {
-                                  saveOffset();
-                                }
-                              },
-                        child: isLoading
-                            ? const SizedBox(
-                                width: 25,
-                                height: 25,
-                                child: CircularProgressIndicator(),
-                              )
-                            : Text(
-                                'Request',
-                                style: theme.textTheme.bodyLarge?.copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
