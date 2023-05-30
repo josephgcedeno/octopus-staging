90d60532bdb89b5ff236782342f084c0e78f3e05
josephgcedeno <josephgcedeno@gmail.com>
2023-05-30 13:18:48 +0800

    fix: responsive pdf viewer on hr file

--- lib/module/hr_files/interfaces/screens/pdf_viewer_screen.dart
+++ lib/module/hr_files/interfaces/screens/pdf_viewer_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:octopus/configs/themes.dart';
@@ -8,6 +8,7 @@ import 'package:octopus/infrastructures/models/hr/hr_response.dart';
 import 'package:octopus/interfaces/widgets/appbar.dart';
 import 'package:octopus/interfaces/widgets/widget_loader.dart';
 import 'package:octopus/internal/debug_utils.dart';
+import 'package:octopus/internal/screen_resolution_utils.dart';
 import 'package:octopus/module/hr_files/interfaces/widgets/download_button.dart';
 import 'package:octopus/module/hr_files/services/cubit/hr_cubit.dart';
 import 'package:syncfusion_flutter_core/theme.dart';
@@ -51,107 +52,114 @@ class _PDFViewerScreenState extends State<PDFViewerScreen> {
     return Scaffold(
       appBar: const GlobalAppBar(leading: LeadingButton.back),
       backgroundColor: Colors.white,
-      body: SizedBox(
-        height: height,
-        width: width,
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: width * 0.055,
-                vertical: height * 0.02,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Icon(widget.icon),
-                      Padding(
-                        padding: EdgeInsets.only(left: width * 0.03),
-                        child: Text(
-                          widget.title,
-                          style: kIsWeb
-                              ? theme.textTheme.titleLarge
-                              : theme.textTheme.titleMedium,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: width * 0.055,
+              vertical: height * 0.02,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(widget.icon),
+                    Padding(
+                      padding: EdgeInsets.only(left: width * 0.03),
+                      child: Text(
+                        widget.title,
+                        style: theme.textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ],
-                  ),
-                  if (urlPDF != null)
-                    DownloadButton(
-                      title: widget.title,
-                      url: urlPDF!,
-                      documentLoading: isLoading,
                     ),
-                ],
-              ),
+                  ],
+                ),
+                if (urlPDF != null)
+                  DownloadButton(
+                    title: widget.title,
+                    url: urlPDF!,
+                    documentLoading: isLoading,
+                  ),
+              ],
             ),
-            Expanded(
-              child: BlocConsumer<HrCubit, HrState>(
-                listenWhen: (HrState previous, HrState current) =>
-                    current is FetchPDFFileFailed ||
-                    current is FetchPDFFileSuccess,
-                listener: (BuildContext context, HrState state) {
-                  if (state is FetchPDFFileSuccess) {
-                    setState(() {
-                      urlPDF = state.companyFiles[0].fileSource;
-                    });
-                  } else if (state is FetchPDFFileFailed) {
-                    showSnackBar(message: state.message);
-                  }
-                },
-                buildWhen: (HrState previous, HrState current) =>
-                    current is FetchPDFFileFailed ||
-                    current is FetchPDFFileLoading ||
-                    current is FetchPDFFileSuccess,
-                builder: (BuildContext context, HrState state) {
-                  if (state is FetchPDFFileSuccess) {
-                    return SfPdfViewerTheme(
-                      data: SfPdfViewerThemeData(
-                        progressBarColor: ktransparent,
-                      ),
-                      child: SfPdfViewer.network(
-                        state.companyFiles[0].fileSource,
-                        onDocumentLoadFailed: (_) {
-                          showSnackBar(
-                            message: 'Document failed to load',
-                            snackBartState: SnackBartState.error,
-                          );
-                        },
+          ),
+          Align(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xffFAFBFC),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              width: kIsWeb && width > smWebMinWidth ? width * 0.50 : width,
+              height: height * 0.80,
+              padding: const EdgeInsets.all(10),
+              child: Expanded(
+                child: BlocConsumer<HrCubit, HrState>(
+                  listenWhen: (HrState previous, HrState current) =>
+                      current is FetchPDFFileFailed ||
+                      current is FetchPDFFileSuccess,
+                  listener: (BuildContext context, HrState state) {
+                    if (state is FetchPDFFileSuccess) {
+                      setState(() {
+                        urlPDF = state.companyFiles[0].fileSource;
+                      });
+                    } else if (state is FetchPDFFileFailed) {
+                      showSnackBar(message: state.message);
+                    }
+                  },
+                  buildWhen: (HrState previous, HrState current) =>
+                      current is FetchPDFFileFailed ||
+                      current is FetchPDFFileLoading ||
+                      current is FetchPDFFileSuccess,
+                  builder: (BuildContext context, HrState state) {
+                    if (state is FetchPDFFileSuccess) {
+                      return SfPdfViewerTheme(
+                        data: SfPdfViewerThemeData(
+                          progressBarColor: ktransparent,
+                        ),
+                        child: SfPdfViewer.network(
+                          state.companyFiles[0].fileSource,
+                          onDocumentLoadFailed: (_) {
+                            showSnackBar(
+                              message: 'Document failed to load',
+                              snackBartState: SnackBartState.error,
+                            );
+                          },
+                        ),
+                      );
+                    } else if (state is FetchPDFFileFailed) {
+                      showSnackBar(message: state.message);
+                    }
+                    return Container(
+                      width: double.infinity,
+                      color: kLightGrey,
+                      padding: EdgeInsets.all(width * 0.035),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          for (int i = 0; i < loaderItems; i++)
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                vertical: height * 0.008,
+                                horizontal: width * 0.025,
+                              ),
+                              child: lineLoader(
+                                height: Random().nextInt(15) + 10,
+                                width: double.infinity,
+                              ),
+                            )
+                        ],
                       ),
                     );
-                  } else if (state is FetchPDFFileFailed) {
-                    showSnackBar(message: state.message);
-                  }
-                  return Container(
-                    width: double.infinity,
-                    color: kLightGrey,
-                    padding: EdgeInsets.all(width * 0.035),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        for (int i = 0; i < loaderItems; i++)
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                              vertical: height * 0.008,
-                              horizontal: width * 0.025,
-                            ),
-                            child: lineLoader(
-                              height: Random().nextInt(15) + 10,
-                              width: double.infinity,
-                            ),
-                          )
-                      ],
-                    ),
-                  );
-                },
+                  },
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
