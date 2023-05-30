5ebff89e1bf0ee5e4d8899f9cc8885ff95c88965
josephgcedeno <josephgcedeno@gmail.com>
2023-05-30 11:49:03 +0800

    fix: unable to load pdf

--- lib/module/hr_files/interfaces/screens/pdf_viewer_screen.dart
+++ lib/module/hr_files/interfaces/screens/pdf_viewer_screen.dart
@@ -31,7 +31,6 @@ class PDFViewerScreen extends StatefulWidget {
 
 class _PDFViewerScreenState extends State<PDFViewerScreen> {
   bool isLoading = true;
-  bool isFailed = false;
 
   String? urlPDF;
 
@@ -49,137 +48,109 @@ class _PDFViewerScreenState extends State<PDFViewerScreen> {
 
     const int loaderItems = 13;
 
-    return BlocListener<HrCubit, HrState>(
-      listenWhen: (HrState previous, HrState current) =>
-          current is FetchPDFFileLoading ||
-          current is FetchPDFFileFailed ||
-          current is FetchPDFFileSuccess,
-      listener: (BuildContext context, HrState state) {
-        if (state is FetchPDFFileLoading) {
-          setState(() {
-            isLoading = true;
-            urlPDF = null;
-          });
-        } else if (state is FetchPDFFileSuccess) {
-          setState(() {
-            urlPDF = state.companyFiles[0].fileSource;
-          });
-        } else if (state is FetchPDFFileFailed) {
-          showSnackBar(message: state.message);
-        }
-      },
-      child: Scaffold(
-        appBar: const GlobalAppBar(leading: LeadingButton.back),
-        backgroundColor: Colors.white,
-        body: SizedBox(
-          height: height,
-          width: width,
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: width * 0.055,
-                  vertical: height * 0.02,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Icon(widget.icon),
-                        Padding(
-                          padding: EdgeInsets.only(left: width * 0.03),
-                          child: Text(
-                            widget.title,
-                            style: kIsWeb
-                                ? theme.textTheme.titleLarge
-                                : theme.textTheme.titleMedium,
-                          ),
+    return Scaffold(
+      appBar: const GlobalAppBar(leading: LeadingButton.back),
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: height,
+        width: width,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: width * 0.055,
+                vertical: height * 0.02,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(widget.icon),
+                      Padding(
+                        padding: EdgeInsets.only(left: width * 0.03),
+                        child: Text(
+                          widget.title,
+                          style: kIsWeb
+                              ? theme.textTheme.titleLarge
+                              : theme.textTheme.titleMedium,
                         ),
+                      ),
+                    ],
+                  ),
+                  if (urlPDF != null)
+                    DownloadButton(
+                      title: widget.title,
+                      url: urlPDF!,
+                      documentLoading: isLoading,
+                    ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: BlocConsumer<HrCubit, HrState>(
+                listenWhen: (HrState previous, HrState current) =>
+                    current is FetchPDFFileFailed ||
+                    current is FetchPDFFileSuccess,
+                listener: (BuildContext context, HrState state) {
+                  if (state is FetchPDFFileSuccess) {
+                    setState(() {
+                      urlPDF = state.companyFiles[0].fileSource;
+                    });
+                  } else if (state is FetchPDFFileFailed) {
+                    showSnackBar(message: state.message);
+                  }
+                },
+                buildWhen: (HrState previous, HrState current) =>
+                    current is FetchPDFFileFailed ||
+                    current is FetchPDFFileLoading ||
+                    current is FetchPDFFileSuccess,
+                builder: (BuildContext context, HrState state) {
+                  if (state is FetchPDFFileSuccess) {
+                    return SfPdfViewerTheme(
+                      data: SfPdfViewerThemeData(
+                        progressBarColor: ktransparent,
+                      ),
+                      child: SfPdfViewer.network(
+                        state.companyFiles[0].fileSource,
+                        onDocumentLoadFailed: (_) {
+                          showSnackBar(
+                            message: 'Document failed to load',
+                            snackBartState: SnackBartState.error,
+                          );
+                        },
+                      ),
+                    );
+                  } else if (state is FetchPDFFileFailed) {
+                    showSnackBar(message: state.message);
+                  }
+                  return Container(
+                    width: double.infinity,
+                    color: kLightGrey,
+                    padding: EdgeInsets.all(width * 0.035),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        for (int i = 0; i < loaderItems; i++)
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: height * 0.008,
+                              horizontal: width * 0.025,
+                            ),
+                            child: lineLoader(
+                              height: Random().nextInt(15) + 10,
+                              width: double.infinity,
+                            ),
+                          )
                       ],
                     ),
-                    if (urlPDF != null)
-                      DownloadButton(
-                        title: widget.title,
-                        url: urlPDF!,
-                        documentLoading: isLoading,
-                      ),
-                  ],
-                ),
+                  );
+                },
               ),
-              Expanded(
-                child: Stack(
-                  children: <Widget>[
-                    if (urlPDF != null)
-                      SfPdfViewerTheme(
-                        data: SfPdfViewerThemeData(
-                          progressBarColor: ktransparent,
-                        ),
-                        child: SfPdfViewer.network(
-                          urlPDF!, // pdf file URL
-                          onDocumentLoaded: (_) {
-                            setState(() {
-                              isLoading = false;
-                            });
-                          },
-                          onDocumentLoadFailed: (_) {
-                            setState(() {
-                              isFailed = true;
-                            });
-                          },
-                        ),
-                      ),
-                    Visibility(
-                      visible: isLoading,
-                      child: Container(
-                        width: double.infinity,
-                        color: kLightGrey,
-                        padding: EdgeInsets.all(width * 0.035),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            for (int i = 0; i < loaderItems; i++)
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: height * 0.008,
-                                  horizontal: width * 0.025,
-                                ),
-                                child: lineLoader(
-                                  height: Random().nextInt(15) + 10,
-                                  width: double.infinity,
-                                ),
-                              )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Visibility(
-                      visible: isFailed,
-                      child: Container(
-                        width: double.infinity,
-                        color: kLightGrey,
-                        padding: EdgeInsets.all(width * 0.035),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.all(height * 0.015),
-                              child: const Icon(
-                                Icons.error_outline_outlined,
-                              ),
-                            ),
-                            const Text('Document failed to load'),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
